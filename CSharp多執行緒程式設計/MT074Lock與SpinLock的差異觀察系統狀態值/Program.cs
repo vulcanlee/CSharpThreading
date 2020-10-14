103a0ba9f68a2ef85315bc625f06b1ffe7c821e4
Vulcan <vulcan.lee@gmail.com>
2020-10-14 23:18:12 +0800

    MT074|Lock 與 SpinLock 的差異，觀察系統狀態值

---
+++ CSharp多執行緒程式設計/MT074Lock與SpinLock的差異觀察系統狀態值/Program.cs
@@ -0,0 +1,53 @@
+﻿//#define LOCK
+#define SPINLOCK
+using System;
+using System.Threading;
+
+namespace MT074Lock與SpinLock的差異觀察系統狀態值
+{
+    class Program
+    {
+        static object obj = new object();
+        static SpinLock spinLock = new SpinLock();
+#if LOCK
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(_ =>
+            {
+                Thread.Sleep(1000);
+                locker();
+            });
+            thread.Start();
+            locker();
+        }
+
+        static void locker()
+        {
+            lock (obj)
+            {
+                Thread.Sleep(60 * 1000);
+            }
+        }
+#endif
+#if SPINLOCK
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(_ =>
+            {
+                Thread.Sleep(1000);
+                locker();
+            });
+            thread.Start();
+            locker();
+        }
+
+        static void locker()
+        {
+            bool lockTaken = false;
+            spinLock.Enter(ref lockTaken);
+            Thread.Sleep(60 * 1000);
+            spinLock.Exit();
+        }
+#endif
+    }
+}
