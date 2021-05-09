174e461cb71d6f94c55391cf60514a83a508b948
Vulcan <vulcan.lee@gmail.com>
2021-05-09 20:33:30 +0800

    MT084|各種非同步工作的建立與啟動方法

---
+++ CSharp多執行緒程式設計/MT084各種非同步工作的建立與啟動方法/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MT084各種非同步工作的建立與啟動方法
+{
+    // 這個範例展示了各種非同步工作的建立與啟動方法
+    // 這個範例中，並沒有傳入任何參數到委派方法內
+    // 
+    // 可以嘗試將 MyTask() 與 MyTask有參數() 內的 Thread.Sleep(500) 移除，看看執行結果會如何?
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 方法1 : 使用執行緒的集區   .NET 2 開始提供
+            // 建立完成後，該非同步工作，就會自動啟動
+            ThreadPool.QueueUserWorkItem(state => 我的非同步方法());
+
+            // 方法2 : 使用 Task 類別，在建立物件的時候，傳入非同步的委派方法   .NET 4 開始提供
+            // 產生完 Task 物件之後，需要呼叫 Start() 方法，來啟動這個非同步委派方法
+            var t = new Task(我的非同步方法);
+            t.Start();
+            var t2 = new Task(() => 我的非同步方法());
+            t2.Start();
+
+            // 方法3 - 使用 Task.Factory 靜態方法產生工作，這些 Factory 方法用於建立及設定 Task 和 Task<TResult> 執行個體。
+            //         等同於建立藉由呼叫無參數的類別 TaskFactory.TaskFactory() 建構函式
+            // 使用 Task.Factory 靜態方法產生的非同步工作，會立即開始執行
+            Task.Factory.StartNew(我的非同步方法);
+            Task.Factory.StartNew(() => 我的非同步方法());
+
+            // 方法4 - 將指定在 ThreadPool 執行工作排入佇列，並傳回該工作的工作控制代碼    .NET 4.5 的 Task 類別新增了靜態方法 Run。
+            //         Run 方法可讓您建立及執行工作的單一方法呼叫中，簡單的替代方案 StartNew 方法。 
+            //         它會建立工作以下列的預設值：
+            //               其取消語彙基元是 CancellationToken.None。
+            //               其 CreationOptions 屬性值是 TaskCreationOptions.DenyChildAttach。
+            //               它會使用預設工作排程器。
+            // TaskTask.Run 執行後，會立即開始執行非同的委派工作
+            Task.Run(() => 我的非同步方法());
+
+            Console.WriteLine("按下任一按鍵，結束處理程序");
+            Console.ReadKey();
+        }
+
+        static void 我的非同步方法()
+        {
+            Console.WriteLine("執行 非同步方法, 沒有 傳入參數的工作");
+            Console.WriteLine("  工作執行緒 #{0}", Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(500);
+        }
+
+    }
+}
