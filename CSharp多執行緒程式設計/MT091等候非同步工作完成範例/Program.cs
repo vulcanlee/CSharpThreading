32220909fa690e8b78291cfa81ee8d8d60213c95
Vulcan <vulcan.lee@gmail.com>
2021-05-14 23:06:27 +0800

    MT091|等候非同步工作完成範例

---
+++ CSharp多執行緒程式設計/MT091等候非同步工作完成範例/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MT091等候非同步工作完成範例
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"主執行緒執行中");
+            Task task = Task.Run(() =>
+            {
+                Console.WriteLine($"  非同步工作正在執行中");
+                Task.Delay(3000).Wait();
+                Console.WriteLine($"  非同步工作執行完成");
+            });
+            Console.WriteLine($"等候非同步工作完成");
+            task.Wait();
+            Console.WriteLine($"主執行緒執行完成");
+
+        }
+    }
+}
